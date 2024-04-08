b541e058fabb1937386398c3b9d8aa8b72fb14c8
Nikhil <nikhilrawal13101999@gmail.com>
2024-04-08 19:25:06 +0530

    add: Advice generator app with deploy file

---
+++ README.md
@@ -0,0 +1,68 @@
+# Advice generator App
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Useful Resources](#useful-resources)
+- [Author](#author)
+- [Feedback](#feedback)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
+
+### Screenshot
+
+![](../images/advice-generator-app.jpg)
+ 
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [TailwindCSS](https://tailwindcss.com/) - For styles
+- [Vite](https://vitejs.dev/) - Open-source JavaScript module bundler
+
+
+### What I learned
+
+I learned to fetech data using fetech API from api.adviceslip.com. Implemented design styles for better user interface and smooth transition.
+
+### Useful resources
+
+- [Font Awesome](https://fontawesome.com/) - This site gave me all kind of icons/logo in required type . I really liked this site and will be using it.
+
+
+## Author
+
+- Website - [Nikhil R](https://nikhil-131.github.io/portfolio/)
+- LinkedIn - [@nikhil-r-rawal](https://www.linkedin.com/in/nikhil-r-rawal)
+- Twitter - [@NikhilRRawal](https://www.twitter.com/NikhilRRawal)
+
+
+## Feedback
+
+I would greatly appreciate feedback on the advice generator web application. Your insights and suggestions would be invaluable for further enhancement.
---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import desktop from "./assets/images/desktop.svg"
+import mobile from "./assets/images/mobile.svg"
+import dice from "./assets/images/dice.svg"
+
+function App() {
+  const [adviceslip, setAdviceslip] = useState({
+    id: '',
+    advice: ''
+  });
+  useEffect(() => {
+    // alert("first");
+    (async () => {
+      let api = await fetch("https://api.adviceslip.com/advice");
+      let response = await api.json();
+      setAdviceslip(response.slip);
+    })();
+  }, [])
+
+  const handleClick = async (event) => {
+    let api = await fetch("https://api.adviceslip.com/advice");
+    let response = await api.json();
+    setAdviceslip(response.slip);
+  }
+
+  console.log(adviceslip)
+  return (
+    <>
+      <div className="container lg:w-[40%] md:w-[60%] h-fit  bg-[#323a49] absolute top-0 bottom-0 left-0 right-0 m-auto rounded-2xl p-6 flex items-center justify-center flex-col">
+        <div className="adviceID flex justify-center items-center">
+          <p className='text-[#52ffa8] text-sm tracking-[0.25rem]'>ADVICE #{adviceslip.id}</p>
+        </div>
+        <div className="advice flex justify-center items-center text-center font-bold px-10 py-8">
+          <h1 className='text-white text-3xl'>“{adviceslip.advice}”</h1>
+        </div>
+        <div className="designImg pb-7">
+          <picture>
+            <source media="(max-width: 640px)" srcSet={mobile} />
+            <img src={desktop} alt="" />
+          </picture>
+        </div>
+        <div className="dice flex justify-center">
+          <button onClick={handleClick} className='bg-[#52ffa8] p-4 rounded-full absolute bottom-[-28px] transition-all ease-in-out duration-300 hover:shadow-[0px_0px_55px_5px_#52ffa8]'><img src={dice} alt="dice" /></button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
